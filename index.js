5ccefa3fd967609c53850859a841fc0ccfa1383c
Tim Stallard <code@timstallard.me.uk>
2015-12-19 14:20:06 +0000

    Added start of MQTT

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var mongojs = require("mongojs");
 var db = mongojs("sherry", ["options", "votes"]);
+var mqtt = require("mqtt");
+
+var mqttclient = mqtt.connect("mqtt://localhost");
 
 var app = express();
 var server = app.listen(9001);
 var io = require("socket.io").listen(server);
+
+mqttclient.publish("test", "hello world");
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/client/index.html");
@@ -13,7 +18,7 @@ app.get("/", function(req, res){
 app.get("/admin/options/", function(req, res){
   db.options.find(function(err, items){
     res.json(items);
-  })
+  });
 });
 
 app.delete("/admin/options/:id", function(req, res){
@@ -30,6 +35,14 @@ app.get("/vote/:id", function(req, res){
   db.options.update({_id: db.ObjectId(req.params.id)}, {$inc: {votes: 1}});
   db.options.find({}, function(err, items){
     io.emit("data", items);
+    items.forEach(function(item, itemid){
+      console.log("items");
+      item.colour.forEach(function(colour, colourid){
+          for(var i = 0; i < item.votes; i++){
+            mqttclient.publish("led/" + (itemid + i) + "/" + colourid, colour.toString());
+          }
+      });
+    });
   });
   res.sendStatus(200);
 });
