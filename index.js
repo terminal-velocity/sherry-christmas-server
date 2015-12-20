1b415159ae185d1db2c2f6cbe8cdf4650d6be42b
Tim Stallard <code@timstallard.me.uk>
2015-12-20 04:28:19 +0000

    Implemented admin system, made many front and back end changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var mongojs = require("mongojs");
-var db = mongojs("192.168.3.97/sherry", ["options", "votes"]);
+var db = mongojs("192.168.3.97/sherry", ["options", "votes", "question"]);
 var mqtt = require("mqtt");
 var smooth = require("./smooth.js");
+var bodyparser = require("body-parser");
 
 var mqttclient = mqtt.connect("mqtt://192.168.3.97");
 
@@ -10,23 +11,61 @@ var app = express();
 var server = app.listen(9001);
 var io = require("socket.io").listen(server);
 
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
 mqttclient.publish("test", "hello world");
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/client/index.html");
 });
 
-app.get("/options/", function(req, res){
-  db.options.find(function(err, items){
-    res.json(items);
-  });
+app.get("/admin", function(req, res){
+  res.sendFile(__dirname + "/client/admin.html");
 });
+
+app.route("/options")
+  .get(function(req, res){
+    db.options.find(function(err, items){
+      res.json(items);
+    });
+  })
+  .post(function(req, res){
+    var colour = [parseInt(req.body.colour[0]), parseInt(req.body.colour[1]), parseInt(req.body.colour[2])]
+    db.options.insert({
+      name: req.body.name,
+      colour: colour
+    }, function(){
+      res.sendStatus(200);
+    });
+  });
 
 app.delete("/options/:id", function(req, res){
   db.options.remove({_id: db.ObjectId(req.params.id)}, function(){
     res.sendStatus(200);
   });
 });
+
+app.route("/question")
+  .get(function(req, res){
+    db.question.find({}, function(err, questions){
+      if(questions.length){
+        res.send(questions[0].question);
+      }
+      else{
+        res.send("");
+      }
+    });
+  })
+  .post(function(req, res){
+    db.question.update({}, {
+      $set: {
+        question: req.body.question
+      }
+    }, function(){
+      res.sendStatus(200);
+    });
+  });
 
 app.get("/vote/:id", function(req, res){
   db.options.update({_id: db.ObjectId(req.params.id)}, {$inc: {votes: 1}}, function(){
@@ -58,11 +97,11 @@ function sendvotestotree(){
       var leds = [];
       for(var i = 0; i < 50; i++){
         var data = smoothfunction(i);
-        leds.push(Math.round(data[0]), Math.round(data[1] * 0.6), Math.round(data[2] * 0.75));
+        leds.push([Math.round(data[0]), Math.round(data[1] * 0.6), Math.round(data[2] * 0.75)]);
       }
       var ledvalues = [];
       leds.forEach(function(led){
-        ledvalues.push(led);
+        ledvalues = ledvalues.concat(led);
       });
       mqttclient.publish("led", ledvalues.join(",") + ",");
     });
